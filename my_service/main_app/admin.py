3c83f67ac8f147ca6b5427f5cd84ab31c53f7339
svyatoslav <yoko11.06.92@yandex.ru>
2020-11-30 23:06:35 +0400

    переименовал модели

--- my_service/main_app/admin.py
+++ my_service/main_app/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
-from main_app.models import ReferenceBook, RefBookItem
+from main_app.models import Glossary, GlossaryItem
 
 
-@admin.register(ReferenceBook)
-class CartAdmin(admin.ModelAdmin):
+@admin.register(Glossary)
+class GlossaryAdmin(admin.ModelAdmin):
     list_display = ['short_name', 'version', 'initial_date']
 
 
-@admin.register(RefBookItem)
-class CartAdmin(admin.ModelAdmin):
+@admin.register(GlossaryItem)
+class GlossaryItemAdmin(admin.ModelAdmin):
     list_display = ['article', 'ref_book']
--- my_service/main_app/models.py
+++ my_service/main_app/models.py
@@ -3,21 +3,26 @@ from django.db import models
 
 # Create your models here.
 
-class ReferenceBook(models.Model):
+class Glossary(models.Model):
     # справочник
+    VERSIONS = (
+        ('0.1', '1'),
+        ('0.2', '2'),
+        ('0.3', '3'),
+    )
     name = models.CharField(max_length=255)
     short_name = models.CharField(max_length=50)
     description = models.TextField()
-    version = models.CharField(max_length=10, unique=True)
+    version = models.CharField(max_length=10, choices=VERSIONS)
     initial_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}'.format(self.short_name)
 
 
-class RefBookItem(models.Model):
+class GlossaryItem(models.Model):
     # элемент справочника
-    ref_book = models.ForeignKey(ReferenceBook, related_name='item', on_delete=models.CASCADE)
+    ref_book = models.ForeignKey(Glossary, related_name='item', on_delete=models.CASCADE)
     article = models.CharField(max_length=50)
     value = models.CharField(max_length=255)
 
