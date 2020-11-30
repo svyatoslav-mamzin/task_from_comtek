06b11fa7c8c05a0a6bd696ac33183f9a3b669ebc
svyatoslav <yoko11.06.92@yandex.ru>
2020-11-30 16:38:11 +0400

    добавил сущности

--- my_service/main_app/admin.py
+++ my_service/main_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from main_app.models import ReferenceBook, RefBookItem
+
+
+@admin.register(ReferenceBook)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'version', 'initial_date']
+
+
+@admin.register(RefBookItem)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['article', 'ref_book']
--- my_service/main_app/models.py
+++ my_service/main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ReferenceBook(models.Model):
+    # справочник
+    name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=50)
+    description = models.TextField()
+    version = models.CharField(max_length=10, unique=True)
+    initial_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.short_name)
+
+
+class RefBookItem(models.Model):
+    # элемент справочника
+    ref_book = models.ForeignKey(ReferenceBook, related_name='item', on_delete=models.CASCADE)
+    article = models.CharField(max_length=50)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
